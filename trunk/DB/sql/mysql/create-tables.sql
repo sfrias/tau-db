15b54cc9e960f9034b714a6f3648fffc342df1f6
hila.barzilai <hila.barzilai@0a486642-f744-989d-f3b0-5f899da6afa2>
2011-04-20 09:50:25 +0000

    updated sql files and utils

--- trunk/DB/sql/mysql/create-tables.sql
+++ trunk/DB/sql/mysql/create-tables.sql
@@ -1,15 +1,6 @@
-create table CHARACTERS
-  (CHAR_ID integer NOT NULL,
-  CHAR_NAME varchar(40) NOT NULL,
-  PRIMARY KEY (CHAR_ID));
-  
- create table FICTIONAL_ORGANIZATIONS
- 	(ORG_NAME varchar(40) NOT NULL,
- 	ORG_ID varchar(40) NOT NULL,
- 	ORG_MEMBER varchar(40),
- 	ORG_TYPE varchar(40),
- 	ORG_APPEARS varchar(40),
- 	ORG_FOUNDER varchar(40),
- 	ORG_PARENT_ORG varchar(40),
- 	ORG_SUB_ORG varchar(40),
- 	PRIMARY KEY (ORG_ID));
+CREATE TABLE species (
+  `species_id` int(11) NOT NULL AUTO_INCREMENT,
+  `species_fb_id` varchar(40) NOT NULL,
+  `species_name` varchar(40) NOT NULL,
+  PRIMARY KEY (`species_id`)
+)
