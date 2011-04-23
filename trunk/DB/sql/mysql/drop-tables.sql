da91db2792f1f7bff099d23af04e1659e128a264
hila.barzilai <hila.barzilai@0a486642-f744-989d-f3b0-5f899da6afa2>
2011-04-23 19:28:06 +0000

    updateCharactersAndUniverseTable method in JDCBTutorialUtils not working properly yet

--- trunk/DB/sql/mysql/drop-tables.sql
+++ trunk/DB/sql/mysql/drop-tables.sql
@@ -1,22 +1,11 @@
-
 DROP TABLE IF EXISTS characters;
-
-
 DROP TABLE IF EXISTS locations;
-
 DROP TABLE IF EXISTS place_of_birth;
-
-
 DROP TABLE IF EXISTS universe;
-
-
-
-
 DROP TABLE IF EXISTS species;
 DROP TABLE IF EXISTS creator;
 DROP TABLE IF EXISTS organization;
 DROP TABLE IF EXISTS gender;
-
 DROP TABLE IF EXISTS school;
 DROP TABLE IF EXISTS rank;
 DROP TABLE IF EXISTS ethnicity;
---
+++ trunk/DB/src/Utils/JDCConnection.java
@@ -0,0 +1,331 @@
+package Utils;
+
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.CallableStatement;
+import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.NClob;
+import java.sql.PreparedStatement;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.SQLXML;
+import java.sql.Savepoint;
+import java.sql.Statement;
+import java.sql.Struct;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * This is not our original code
+ * @author http://java.sun.com/developer/onlineTraining/Programming/JDCBook/conpool.html
+ *
+ */
+public class JDCConnection implements Connection {
+
+    private JDCConnectionPool pool;
+    private Connection conn;
+    private boolean inuse;
+    private long timestamp;
+
+
+    public JDCConnection(Connection conn, JDCConnectionPool pool) {
+        this.conn=conn;
+        this.pool=pool;
+        this.inuse=false;
+        this.timestamp=0;
+    }
+
+    public synchronized boolean lease() {
+       if(inuse)  {
+           return false;
+       } else {
+          inuse=true;
+          timestamp=System.currentTimeMillis();
+          return true;
+       }
+    }
+    public boolean validate() {
+	try {
+            conn.getMetaData();
+        }catch (Exception e) {
+	    return false;
+	}
+	return true;
+    }
+
+    public boolean inUse() {
+        return inuse;
+    }
+
+    public long getLastUse() {
+        return timestamp;
+    }
+
+    public void close() throws SQLException {
+       pool.returnConnection(this);
+    }
+
+    protected void expireLease() {
+        inuse=false;
+    }
+
+    protected Connection getConnection() {
+        return conn;
+    }
+
+    public PreparedStatement prepareStatement(String sql) throws SQLException {
+        return conn.prepareStatement(sql);
+    }
+
+    public CallableStatement prepareCall(String sql) throws SQLException {
+        return conn.prepareCall(sql);
+    }
+
+    public Statement createStatement() throws SQLException {
+        return conn.createStatement();
+    }
+
+    public String nativeSQL(String sql) throws SQLException {
+        return conn.nativeSQL(sql);
+    }
+
+    public void setAutoCommit(boolean autoCommit) throws SQLException {
+        conn.setAutoCommit(autoCommit);
+    }
+
+    public boolean getAutoCommit() throws SQLException {
+        return conn.getAutoCommit();
+    }
+
+    public void commit() throws SQLException {
+        conn.commit();
+    }
+
+    public void rollback() throws SQLException {
+        conn.rollback();
+    }
+
+    public boolean isClosed() throws SQLException {
+        return conn.isClosed();
+    }
+
+    public DatabaseMetaData getMetaData() throws SQLException {
+        return conn.getMetaData();
+    }
+
+    public void setReadOnly(boolean readOnly) throws SQLException {
+        conn.setReadOnly(readOnly);
+    }
+  
+    public boolean isReadOnly() throws SQLException {
+        return conn.isReadOnly();
+    }
+
+    public void setCatalog(String catalog) throws SQLException {
+        conn.setCatalog(catalog);
+    }
+
+    public String getCatalog() throws SQLException {
+        return conn.getCatalog();
+    }
+
+    public void setTransactionIsolation(int level) throws SQLException {
+        conn.setTransactionIsolation(level);
+    }
+
+    public int getTransactionIsolation() throws SQLException {
+        return conn.getTransactionIsolation();
+    }
+
+    public SQLWarning getWarnings() throws SQLException {
+        return conn.getWarnings();
+    }
+
+    public void clearWarnings() throws SQLException {
+        conn.clearWarnings();
+    }
+
+	@Override
+	public Array createArrayOf(String typeName, Object[] elements) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Blob createBlob() throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Clob createClob() throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public NClob createNClob() throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public SQLXML createSQLXML() throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Statement createStatement(int resultSetType, int resultSetConcurrency) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Statement createStatement(int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Properties getClientInfo() throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public String getClientInfo(String name) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public int getHoldability() throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Map<String, Class<?>> getTypeMap() throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public boolean isValid(int timeout) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int[] columnIndexes)
+			throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, String[] columnNames) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public void releaseSavepoint(Savepoint savepoint) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public void rollback(Savepoint savepoint) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public void setClientInfo(Properties properties) throws SQLClientInfoException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public void setClientInfo(String name, String value) throws SQLClientInfoException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public void setHoldability(int holdability) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Savepoint setSavepoint() throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Savepoint setSavepoint(String name) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		throw new UnsupportedOperationException();
+		
+	}
+}
