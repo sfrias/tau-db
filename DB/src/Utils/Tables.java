4ca6576c0ffa1d01509e79f048de3eded0b95349
hila.barzilai <hila.barzilai@0a486642-f744-989d-f3b0-5f899da6afa2>
2011-04-28 07:55:21 +0000

    added Tables enum

---
+++ DB/src/Utils/Tables.java
@@ -0,0 +1,21 @@
+package Utils;
+
+public enum Tables {
+	
+	species,
+	creator,
+	organization,
+	gender,
+	universe,
+	locations,
+	place_of_birth,
+	school,
+	rank,
+	ethnicity,
+	occupation,
+	powers,
+	jobs,
+	diseases,
+	characters,
+	characters_and_universes;
+}
