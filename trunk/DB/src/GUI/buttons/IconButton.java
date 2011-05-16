82a90f2f349114ff29a9c58fb8487ffeec8c61d6
barkamir@gmail.com <barkamir@gmail.com@0a486642-f744-989d-f3b0-5f899da6afa2>
2011-05-16 19:38:17 +0000

    GUI - update tab added

--- trunk/DB/src/GUI/buttons/IconButton.java
+++ trunk/DB/src/GUI/buttons/IconButton.java
@@ -1,30 +1,17 @@
 package GUI.buttons;
 
 import java.awt.Font;
-import java.net.URL;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-import GUI.frames.WelcomeScreenFrame;
+import GUI.commons.GuiUtils;
 
 public class IconButton extends JButton {
 	private static final long serialVersionUID = 1L;
 	
-	
-	private static ImageIcon createIcon(String iconName){
-		URL imgURL = WelcomeScreenFrame.class.getResource("../images/" + iconName);
-		if (imgURL != null) {
-	        return new ImageIcon(imgURL, iconName);
-	    } else {
-	        System.err.println("Couldn't find file: " + iconName);
-	        return null;
-	    }
-	}
-	
 	public IconButton(String text, String iconName){
 		super();
-		setIcon(createIcon(iconName));
+		setIcon(GuiUtils.readImageIcon(iconName));
 		setText(text.toUpperCase());
 		setFont(new Font("Footlight MT Light",Font.BOLD,15));
 		setIconTextGap(10);
--- trunk/DB/src/GUI/panels/Manage/Tabs/UpdateTab.java
+++ trunk/DB/src/GUI/panels/Manage/Tabs/UpdateTab.java
@@ -1,28 +1,222 @@
 package GUI.panels.Manage.Tabs;
 
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
-import GUI.panels.Manage.cards.add.AddGender;
-import GUI.panels.Manage.cards.add.AddOccupation;
+import Enums.UpdateResult;
+import GUI.GuiHandler;
+import GUI.buttons.IconButton;
+import GUI.commons.GuiUtils;
+import GUI.workers.UpdateWorker;
 
-public class UpdateTab extends GenericTab{
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+public class UpdateTab extends JPanel implements PropertyChangeListener{
 	private static final long serialVersionUID = 1L;
-	
+
+	private DescriptionPanel descDownload;
+	private DescriptionPanel descExtract;
+	private DescriptionPanel descUpdate;
+	private DescriptionPanel descDelete;
+
+	private UpdateWorker worker = null;
+	private JButton cancelButton ;
+	private IconButton startImportButton;
+	private JProgressBar progressBar;
+	private UpdateTab me = this;
+
 	public UpdateTab(){
 		super();
+
+		buildTab();
+	}
+
+	private void buildTab(){
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+		startImportButton = new IconButton("start import", "import.png");
+		startImportButton.setAlignmentX(CENTER_ALIGNMENT);
+		startImportButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				startImportButton.setEnabled(false);
+				worker = new UpdateWorker(me);
+				worker.addPropertyChangeListener(me);
+				worker.execute();
+				cancelButton.setEnabled(true);
+				progressBar.setString("downloading in progress");
+
+			}
+
+		});
+
+		cancelButton = new JButton("Cancel");
+		cancelButton.setEnabled(false);
+		cancelButton.setFont(new Font("Footlight MT Light",Font.BOLD,15));
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (worker != null){
+					worker.cancel(true);
+					worker = null;
+				}				
+			}
+		})  ;
+		cancelButton.setAlignmentX(CENTER_ALIGNMENT);
+
+		JPanel progressPanel = createMainProgressPanel();
+		progressPanel.setAlignmentX(CENTER_ALIGNMENT);
+
+		progressBar = new JProgressBar(0, 100);
+		progressBar.setMaximumSize(new Dimension(400,20));
+		progressBar.setStringPainted(true);
+		
+		add(Box.createRigidArea(new Dimension(0, 25)));
+		add(startImportButton);
+		add(Box.createRigidArea(new Dimension(0, 15)));
+		add(cancelButton);
+		add(Box.createRigidArea(new Dimension(0, 35)));
+		add(progressPanel);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(progressBar);
+		add(Box.createRigidArea(new Dimension(0, 15)));
+		setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+	}
+
+	private JPanel createMainProgressPanel(){
+		descDownload = new DescriptionPanel("download dumps file");
+		descExtract = new DescriptionPanel("files extracting");
+		descUpdate = new DescriptionPanel("tables updating");
+		descDelete = new DescriptionPanel("remove temporary files");
+
+		FormLayout layout = new FormLayout("5dlu, left:pref","p, 4dlu, p, 4dlu, p, 4dlu, p");
+		PanelBuilder builder = new PanelBuilder(layout);
+		builder.setDefaultDialogBorder();
+		CellConstraints cc = new CellConstraints();
+
+		builder.add(descDownload, cc.xy(2,1));
+		builder.add(descExtract, cc.xy(2,3));
+		builder.add(descUpdate, cc.xy(2,5));
+		builder.add(descDelete, cc.xy(2,7));		
+
+		JPanel panel = builder.getPanel();
+		panel.setMaximumSize(new Dimension(360,200));
+
+		return panel;
+	}
+
+	public void refreshTab(UpdateResult progress){
+		switch (progress){
+		case finish_download:
+			descDownload.setEnabled(true);
+			progressBar.setString("extracting in progress");
+			break;
+		case finish_extract:
+			descExtract.setEnabled(true);
+			break;
+		case start_update_table:
+			cancelButton.setEnabled(false);
+			progressBar.setString("updating in progress");
+			break;
+		case finish_update_table:
+			descUpdate.setEnabled(true);
+			progressBar.setString("deleting in progress");
+			break;
+		case finish_delete:
+			descDelete.setEnabled(true);
+			break;
+		case cancel_accepted:
+			GuiHandler.showCancelOperationDialog();
+			clearProgress();
+			break;
+		case done:
+			progressBar.setString("update completed");
+			GuiHandler.showUpdateCompleteDialog();
+			clearProgress();
+			break;
+		case exception:
+			GuiHandler.showResultExceptionDialog("update");
+			clearProgress();
+			break;
+		}
+	}
+
+	private void clearProgress(){
+		descDownload.setEnabled(false);
+		descExtract.setEnabled(false);
+		descUpdate.setEnabled(false);
+		descDelete.setEnabled(false);
+		cancelButton.setEnabled(true);
+		progressBar.setValue(0);
+		progressBar.setString(null);
+		startImportButton.setEnabled(true);
+		cancelButton.setEnabled(false);
+		GuiHandler.stopStatusFlash();
+	}
+
+
+
+	class DescriptionPanel extends JPanel{
+		private static final long serialVersionUID = 1L;
+
+		private String description;
+		private JLabel descriptionLabel;
+		private JLabel statusIcon ;
+
+		public DescriptionPanel(String description){
+			super();
+
+			this.description = description;
+			descriptionLabel = new JLabel(description);
+			descriptionLabel.setFont(new Font("Footlight MT Light",Font.BOLD,20));
+			descriptionLabel.setEnabled(false);
+			descriptionLabel.setAlignmentX(LEFT_ALIGNMENT);
+
+			statusIcon = new JLabel(GuiUtils.readImageIcon("questionmark.png"));
+			statusIcon.setAlignmentX(LEFT_ALIGNMENT);
+
+			add(statusIcon);
+			add(Box.createRigidArea(new Dimension(10,0)));
+			add(descriptionLabel);
+		}
+
+		@Override
+		public void setEnabled(boolean b){
+			super.setEnabled(b);
+
+			if (b){
+				descriptionLabel.setEnabled(true);
+				descriptionLabel.setText(description + "...Done!");
+				statusIcon.setIcon(GuiUtils.readImageIcon("checked.png"));
+			}else {
+				descriptionLabel.setEnabled(false);
+				descriptionLabel.setText(description);
+				statusIcon.setIcon(GuiUtils.readImageIcon("questionmark.png"));
+			}
+		}
 	}
 
 	@Override
-	public JPanel addCards(JPanel cards) {
-		cards.add(new AddGender(), "GENDER");
-		cards.add(new AddOccupation(), "NAMES");
-		return cards;
-	}
+	public void propertyChange(PropertyChangeEvent evt) {
+		if (evt.getPropertyName().equals("progress")) {
+			if (worker != null){
+				int progress = worker.getProgress();
+				progressBar.setValue(progress);
+			}
+		} 
 
-	@Override
-	public String getTabAction() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
