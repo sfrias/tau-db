e2ecb4da8911ffda2a5315a11f760954f3ad43f4
barkamir@gmail.com <barkamir@gmail.com@0a486642-f744-989d-f3b0-5f899da6afa2>
2011-05-31 22:19:52 +0000

    added debug for algorithm

--- DB/src/GUI/workers/GenericWorker.java
+++ DB/src/GUI/workers/GenericWorker.java
@@ -33,7 +33,7 @@ public abstract class GenericWorker extends SwingWorker<ResultHolder, Void>{
 	private GenericCardPanel card;
 	private PlayFrame playFrame;
 	private StatisticsTab statisticsTab;
-	
+
 	public GenericWorker(StatisticsTab statisticTab){
 		super();
 		this.statisticsTab = statisticTab;
@@ -84,162 +84,168 @@ public abstract class GenericWorker extends SwingWorker<ResultHolder, Void>{
 
 	@Override
 	public void done(){
-		try {
-			CharacterModel charModel;
+		if (isCancelled()){
 			GuiHandler.stopStatusFlash();
-			ResultHolder result = get();
-			ExecutionResult e = result.getExecutionResult();
-			switch (e){
-			case Success_Simple_Add_Edit_Delete:{
-				GuiHandler.showResultSuccessDialog(action.toString());
-				card.refreshFromModel();
-				break;
-			}
-			case Success_Add_Character:{
-				GuiHandler.showResultSuccessDialog(action.toString());
-				((AddCharacters)card).clearValues();
-				break;
-			}
-			case Success_Simple_Query:{
-				if (card != null){
-					SimpleModel simpleModel = (SimpleModel)result.getModel();
-					if (card instanceof EditSimpleCard){
-						EditSimpleCard editSimpleCard = (EditSimpleCard) card;
-						editSimpleCard.setModel(simpleModel);
-						editSimpleCard.refreshFromModel();
+			GuiHandler.showResultExceptionDialog("TIMER-STOPPED-FOR-DEBUG-ONLY");
+		}else{
+
+			try {
+				CharacterModel charModel;
+				GuiHandler.stopStatusFlash();
+				ResultHolder result = get();
+				ExecutionResult e = result.getExecutionResult();
+				switch (e){
+				case Success_Simple_Add_Edit_Delete:{
+					GuiHandler.showResultSuccessDialog(action.toString());
+					card.refreshFromModel();
+					break;
+				}
+				case Success_Add_Character:{
+					GuiHandler.showResultSuccessDialog(action.toString());
+					((AddCharacters)card).clearValues();
+					break;
+				}
+				case Success_Simple_Query:{
+					if (card != null){
+						SimpleModel simpleModel = (SimpleModel)result.getModel();
+						if (card instanceof EditSimpleCard){
+							EditSimpleCard editSimpleCard = (EditSimpleCard) card;
+							editSimpleCard.setModel(simpleModel);
+							editSimpleCard.refreshFromModel();
+						}
+						else if (card instanceof EditCharacters){
+							EditCharacters editCharacters = (EditCharacters) card;
+							editCharacters.setSimpleModel(simpleModel);
+							editCharacters.refreshFromModel();
+						}
+						else{
+							DeleteCard deleteCard = (DeleteCard) card;
+							deleteCard.setModel(simpleModel);
+							deleteCard.refreshFromModel();
+						}
 					}
-					else if (card instanceof EditCharacters){
+					break;
+				}
+				case Success_Characters_Query:{
+					charModel = (CharacterModel)result.getModel();
+					if (card instanceof AddCharacters){
+						AddCharacters addCharCard = (AddCharacters) card;
+						addCharCard.setModel(charModel);
+						addCharCard.refreshFromModel();
+					}
+					else{
+						EditCharacters editCharCard = (EditCharacters) card;
+						editCharCard.setCharacterModel(charModel);
+						editCharCard.refreshAllAttributesFromCharacterModel();					
+					}
+					break;
+				}
+				case IntegrityConstraintViolationException:
+					GuiHandler.showResultIntegrityDialog();
+					break;
+				case Exception:
+					GuiHandler.showResultExceptionDialog(action.toString());
+					break;
+				case Success_Add_Character_Attribute:{
+					charModel = (CharacterModel)result.getModel();
+					if (card instanceof AddCharacters){
+						AddCharacters addCharCard1 = (AddCharacters) card;
+						addCharCard1.setModel(charModel);
+						addCharCard1.refreshFromModel();
+					}
+					else{
+						EditCharacters editCharCard1 = (EditCharacters) card;
+						editCharCard1.setCharacterModel(charModel);
+						editCharCard1.refreshAllAttributesFromCharacterModel();					
+					}
+					GuiHandler.showResultSuccessDialog(action.toString());
+					break;
+				}
+				case Success_Get_Characters:{
+					SimpleModel simpleModel = (SimpleModel)result.getModel();
+					if (card instanceof EditCharacters){
 						EditCharacters editCharacters = (EditCharacters) card;
 						editCharacters.setSimpleModel(simpleModel);
 						editCharacters.refreshFromModel();
 					}
 					else{
-						DeleteCard deleteCard = (DeleteCard) card;
-						deleteCard.setModel(simpleModel);
-						deleteCard.refreshFromModel();
+						DeleteCharacters deleteCharactersCard = (DeleteCharacters) card;
+						deleteCharactersCard.setModel(simpleModel);
+						deleteCharactersCard.refreshFromModel();
+					}
+					break;
+				}
+				case Success_Get_Characters_For_First_Character:{
+					SimpleModel simpleModel = (SimpleModel)result.getModel();
+					playFrame.setSimpleModel(simpleModel);
+					playFrame.refreshCharIFromModel();
+					break;
+				}
+				case Success_Get_Characters_For_Second_Character:{
+					SimpleModel simpleModel = (SimpleModel)result.getModel();
+					playFrame.setSimpleModel(simpleModel);
+					playFrame.refreshCharIIFromModel();
+					break;
+				}
+				case Success_Get_Character_Attributes:{
+					CharacterModel model = (CharacterModel)result.getModel();
+					EditCharacters editCharactersCard = (EditCharacters) card;
+					editCharactersCard.setCharacterModel(model);
+					editCharactersCard.refreshCharaterAttributesFromCharacterModel();
+					break;
+				}
+				case Success_Get_Character_Attributes_For_First_Character:{
+					CharacterModel model = (CharacterModel)result.getModel();
+					playFrame.refreshPanelLeftDetailsFromModel(model);
+					break;
+				}
+				case Success_Get_Character_Attributes_For_Second_Character:{
+					CharacterModel model = (CharacterModel)result.getModel();
+					playFrame.refresPanelRightDetailsFromModel(model);
+					break;
+				}
+				case Success_Edit_Character:{
+					EditCharacters editCharactersCard = (EditCharacters) card;
+					GuiHandler.showResultSuccessDialog(action.toString());
+					editCharactersCard.clearValues();
+					break;
+				}
+				case Success_Statistics:{
+					StatisticsModel statisticModel = (StatisticsModel) result.getModel();
+					statisticsTab.setModel(statisticModel);
+					statisticsTab.refreshFromModel();
+					break;
+				}
+				case Success_Algorithm:{
+					AlgorithmModel model = (AlgorithmModel) result.getModel();
+					ConnectionResult connResult = model.getConnResult();
+					switch (connResult){
+
+					case Did_Not_Find_Connection:
+					case Found_Connection:
+						GuiHandler.showAlgrithmResultDialog(true,connResult.toString(), model.getResultString());
+						break;
+					case Found_Connection_Of_Length_0:
+						GuiHandler.showAlgrithmResultDialog(false, connResult.toString(), model.getResultString());
+						break;
+
+					default:
+						break;
+					}
+					if (GuiHandler.isStatusFlashing()){
+						GuiHandler.stopStatusFlash();
 					}
 				}
-				break;
-			}
-			case Success_Characters_Query:{
-				charModel = (CharacterModel)result.getModel();
-				if (card instanceof AddCharacters){
-					AddCharacters addCharCard = (AddCharacters) card;
-					addCharCard.setModel(charModel);
-					addCharCard.refreshFromModel();
-				}
-				else{
-					EditCharacters editCharCard = (EditCharacters) card;
-					editCharCard.setCharacterModel(charModel);
-					editCharCard.refreshAllAttributesFromCharacterModel();					
-				}
-				break;
-			}
-			case IntegrityConstraintViolationException:
-				GuiHandler.showResultIntegrityDialog();
-				break;
-			case Exception:
-				GuiHandler.showResultExceptionDialog(action.toString());
-				break;
-			case Success_Add_Character_Attribute:{
-				charModel = (CharacterModel)result.getModel();
-				if (card instanceof AddCharacters){
-					AddCharacters addCharCard1 = (AddCharacters) card;
-					addCharCard1.setModel(charModel);
-					addCharCard1.refreshFromModel();
-				}
-				else{
-					EditCharacters editCharCard1 = (EditCharacters) card;
-					editCharCard1.setCharacterModel(charModel);
-					editCharCard1.refreshAllAttributesFromCharacterModel();					
-				}
-				GuiHandler.showResultSuccessDialog(action.toString());
-				break;
-			}
-			case Success_Get_Characters:{
-				SimpleModel simpleModel = (SimpleModel)result.getModel();
-				if (card instanceof EditCharacters){
-					EditCharacters editCharacters = (EditCharacters) card;
-					editCharacters.setSimpleModel(simpleModel);
-					editCharacters.refreshFromModel();
-				}
-				else{
-					DeleteCharacters deleteCharactersCard = (DeleteCharacters) card;
-					deleteCharactersCard.setModel(simpleModel);
-					deleteCharactersCard.refreshFromModel();
-				}
-				break;
-			}
-			case Success_Get_Characters_For_First_Character:{
-				SimpleModel simpleModel = (SimpleModel)result.getModel();
-				playFrame.setSimpleModel(simpleModel);
-				playFrame.refreshCharIFromModel();
-				break;
-			}
-			case Success_Get_Characters_For_Second_Character:{
-				SimpleModel simpleModel = (SimpleModel)result.getModel();
-				playFrame.setSimpleModel(simpleModel);
-				playFrame.refreshCharIIFromModel();
-				break;
-			}
-			case Success_Get_Character_Attributes:{
-				CharacterModel model = (CharacterModel)result.getModel();
-				EditCharacters editCharactersCard = (EditCharacters) card;
-				editCharactersCard.setCharacterModel(model);
-				editCharactersCard.refreshCharaterAttributesFromCharacterModel();
-				break;
-			}
-			case Success_Get_Character_Attributes_For_First_Character:{
-				CharacterModel model = (CharacterModel)result.getModel();
-				playFrame.refreshPanelLeftDetailsFromModel(model);
-				break;
-			}
-			case Success_Get_Character_Attributes_For_Second_Character:{
-				CharacterModel model = (CharacterModel)result.getModel();
-				playFrame.refresPanelRightDetailsFromModel(model);
-				break;
-			}
-			case Success_Edit_Character:{
-				EditCharacters editCharactersCard = (EditCharacters) card;
-				GuiHandler.showResultSuccessDialog(action.toString());
-				editCharactersCard.clearValues();
-				break;
-			}
-			case Success_Statistics:{
-				StatisticsModel statisticModel = (StatisticsModel) result.getModel();
-				statisticsTab.setModel(statisticModel);
-				statisticsTab.refreshFromModel();
-				break;
-			}
-			case Success_Algorithm:{
-				AlgorithmModel model = (AlgorithmModel) result.getModel();
-				ConnectionResult connResult = model.getConnResult();
-				switch (connResult){
-
-				case Did_Not_Find_Connection:
-				case Found_Connection:
-					GuiHandler.showAlgrithmResultDialog(true,connResult.toString(), model.getResultString());
-					break;
-				case Found_Connection_Of_Length_0:
-					GuiHandler.showAlgrithmResultDialog(false, connResult.toString(), model.getResultString());
-					break;
-					
 				default:
 					break;
-				}
-				if (GuiHandler.isStatusFlashing()){
-					GuiHandler.stopStatusFlash();
-				}
+				}		
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			default:
-				break;
-			}		
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 }
